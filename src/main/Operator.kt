543c2e713146878f8156a2205b77f35d99f93350
cilmesource <46748859+CilmeSourcE@users.noreply.github.com>
2019-10-20 21:07:00 +0900

    演算子について勉強
    ポイントとしては、
    ・三項演算子は使えない
    ・範囲を指定できる演算子がある(in)

--- src/main/Operator.kt
+++ src/main/Operator.kt
@@ -1,5 +1,26 @@
 package main
 
 fun main(args: Array<String>) {
-    // 演算子について勉強
+
+    // Javaでいう三項演算子(fuga? true : false)は使えないみたいですね
+
+    // kotlinでは、inという範囲演算子(SQLのbetweenみたいな?)が使用できる
+    val i = 10
+    // 1~20の範囲
+    if (i in 1..20) {
+        println("iは1~20の範囲内です。")
+    }
+    // ArrayOf(...)を組み合わせて使うこともできる(SQLのinみたいな?)
+    val j = 30
+    // 15, 30, 45のいずれか
+    if (j in arrayOf(15, 30, 45)) {
+        println("jは15, 30, 45のどれかです。")
+    }
+
+    // ビット演算子(Javaでいう & や | など...)
+    // &
+    println(10 and 10)
+    // |
+    println(5 or 50)
+
 }
