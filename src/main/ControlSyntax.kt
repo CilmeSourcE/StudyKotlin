d97c507a4bfb1e32e932640dcbe8f5004ea5a923
cilmesource <46748859+CilmeSourcE@users.noreply.github.com>
2019-10-24 00:26:46 +0900

    ifについて勉強(続き)
    whenという、Javaのswitch文のようなものを勉強した。
    条件式はラムダ式のように記述するので新鮮な気分で書いた。
    
    また、条件に複数の値や範囲値を設定できるのは便利だと思う。
    様々な書き方や一目でわかるような直感的な記述方法が勉強をより楽しくしてくれる。

--- src/main/ControlSyntax.kt
+++ src/main/ControlSyntax.kt
@@ -29,5 +29,93 @@ fun main(args: Array<String>) {
     val z = if (x == 0) "xは0" else if (x == 100) "xは100" else "xは0か100以外"
 
 
+    // 多岐にわたる分岐はwhenを使う
+    // Javaのswitch...caseに似ているが、breakが要らないなど色々違う
+    val point = 200
+    when (point) {
+        2 -> {
+            println("pointは2")
+        }
+        20 -> {
+            println("pointは20")
+        }
+        200 -> {
+            println("pointは200")
+        }
+        // どれにも当てはまらないはelseを書く
+        else -> {
+            println("pointは2, 20, 200以外")
+        }
+    }
 
+    // ifと同様に、代入時に書くこともできる
+    val point2 = when (point) {
+        20 -> {
+            "pointは20"
+        }
+        else -> {
+            "pointは20ではない"
+        }
+    }
+
+
+    //  複数の値や範囲を指定することもできる
+    val testPoint = 95
+    var result = if (testPoint in 80..100) {
+        "80~100"
+    } else {
+        "79以下"
+    }
+
+    // whenも同じような感じ
+    result = when (testPoint) {
+        // 複数を条件にできる
+        99, 100 -> {
+            "99, 100"
+        }
+
+        in 70..98 -> {
+            "70~98"
+        }
+
+        in 50..69 -> {
+            "50~69"
+        }
+        // 否定も使える
+        !in 50..100 -> {
+            "50~100ではない"
+        }
+        else -> {
+            "不明"
+        }
+    }
+
+
+    // 型による分岐も書くことができる(JavaでいうinstanseOf的な)
+    val obj: Any = "ほげほげ"
+    when (obj) {
+        // 型判定後は、それぞれの型に応じたメンバー/演算子を呼び出せる
+        // (スマートキャスト)
+
+        is String -> {
+            println(obj[0])
+        }
+        is Int -> {
+            println(obj)
+        }
+        else -> {
+            "?"
+        }
+    }  // 表示:ほ
+
+
+    // 引数を取らないwhenも書くことができる
+    val xx = 10
+    val yy = 15
+
+    when {
+        xx >= 10 -> println("xxは10以上")
+        xx <= 5 && yy >= 15 -> println("xxは5未満, yyは15以上")
+        else -> println("その他")
+    }
 }
