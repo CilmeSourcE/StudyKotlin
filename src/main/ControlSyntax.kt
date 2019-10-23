ff78e62768ebe2ef1a2a974b4f2b64f617aeb774
cilmesource <46748859+CilmeSourcE@users.noreply.github.com>
2019-10-23 23:36:24 +0900

    制御構文について勉強(if)
    色々と余裕がなかったので、途中段階だけど日が変わる前にコミット。
    ifを式で使えるのがポイント。Javaだと最近switch文が同じような感じで使えたので、それと重ねて覚えておく。

--- src/main/ControlSyntax.kt
+++ src/main/ControlSyntax.kt
@@ -1,5 +1,33 @@
 package main
 
 fun main(args: Array<String>) {
-    // 制御構文 if, forなどを勉強する予定
+
+    // if
+    val num = 108
+    if (num == 0) {
+
+        println("numは0")
+    } else if (num == 100) {
+
+        println("numは100")
+    } else {
+
+        println("numは0でも100でもない")
+    }
+
+    // ifは、変数へ代入するときにも書ける
+    // その場合はelse文を必ず書かないとエラー
+    val x = 100
+    val y = if (x == 0) {
+        "xは0"
+    } else if (x == 100) {
+        "xは100"
+    } else {
+        "xは0以外"
+    }
+    // 一文で書ける場合は、ブロックを省略できる
+    val z = if (x == 0) "xは0" else if (x == 100) "xは100" else "xは0以外"
+
+
+
 }
